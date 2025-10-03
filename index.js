8e74f9c542183550a419c0e098bda3e52874db83
Iveri Atskureli <i.atskureli@gmail.com>
2025-10-04 00:22:28 +0400

    refactor: d.ts file are moved to types directory. improved d.ts files generation process

--- index.js
+++ index.js
@@ -27,10 +27,7 @@
 export { default } from './src/wdk-core/wdk-manager.js'
 
 // Export spec/hrpc
-export { default as HRPC } from './spec/hrpc'
-
-// Export spec/schema
-export * as schema from './spec/schema'
+export { default as HRPC } from './spec/hrpc/index.js'
 
 // Export a react-native-bare-kit compatible bundle that generates on postinstall.
 export { default as bundle } from './bundle/worklet.bundle.mjs'
